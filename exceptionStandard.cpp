fea8bc5bdd12651b315275e304b8db4e25cb7779
zakibn18 <jekben815@gmail.com>
2025-06-09 19:02:22 +0700

    membuat exception standard

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -7,5 +7,22 @@ using namespace std;
 
 int main()
 {
-  
+  cout << "Awal Program" << endl; // penanda 1:...
+  try
+  {
+    array<int, 3> data = {10, 20, 30};
+    // pesan array integer 3 elemen
+    cout << data.at(4) << endl;
+    // memanggil array elemen ke-5
+  }
+  catch (exception &e)
+  {
+    // penangkapan menggunakan objek exception
+    cout << e.what() << endl;
+    /*akan dieksekusi karna array data hanya memiliki 3 elemen*/
+  }
+  cout << "Baris Program Yang terakhir" << endl;
+  /*penanda 2: Bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
+
+  return 0;
 }
