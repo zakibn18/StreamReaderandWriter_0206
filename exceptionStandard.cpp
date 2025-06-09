c660ad64dfe078a6afd342343ab58bbce3d93ef4
zakibn18 <jekben815@gmail.com>
2025-06-09 18:55:04 +0700

    first commit

---
+++ exceptionStandard.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <exception>
+// Untuk obyek exception yang akan digunakan
+#include <array>
+// untuk objek array yang akan kita gunakan 
+using namespace std;
+
+int main()
+{
+  
+}
