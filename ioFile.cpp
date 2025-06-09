2fecabf16c28f8d2b8a88ecf315aa86262ce169e
zakibn18 <jekben815@gmail.com>
2025-06-09 17:46:50 +0700

    membuat output else

--- ioFile.cpp
+++ ioFile.cpp
@@ -47,4 +47,8 @@ int main()
     // tutup file tersebut setelah selesai
     infile.close();
   }
+
+  // jika tidak ditemukan file maka akan menampilkan ini
+  else 
+    cout << "Unable to open file";
 }
