3ce49935f6191c5b9fe7b1bd6e73de90d21681cd
zakibn18 <jekben815@gmail.com>
2025-06-09 18:36:15 +0700

    copy paste

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -15,4 +15,45 @@ int main()
   ofstream outfile;
   // mununjuk ke sebuah nama file
   outfile.open(NamaFile + ".txt", ios::out);
+
+  cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+  
+  // unlimited loop untuk menulis
+  while (true)
+  {
+    cout << "- ";
+    // mendapatkan setiap karakter dalam satu baris
+    getline(cin, baris);
+    // loop akan berhenti jika anda memasukkan karakter q
+    if (baris == "q")
+      break;
+    // menulis dan memasukkan nilia dari 'baris' ke dalam file
+    outfile << baris << endl;
+  } 
+  // selesai dalam menulis sekarang tutup filenya
+  outfile.close();
+
+  // memubuat objek input file stream dalam mode membaca.
+  ifstream infile;
+  // membuka file
+  infile.open(NamaFile + ".txt");
+
+  cout << endl << ">= Membuka dan membaca file " << endl;
+  // jika file ada maka 
+  if (infile.is_open())
+  {
+    // melakukan perulangan setiap baris
+    while (getline(infile, baris))
+    {
+      // dan tampilkan disini
+      cout << baris << '\n';
+    }
+    // tutup file tersebut setelah selesai
+    infile.close();
+  }
+
+  // jika tidak ditemukan file maka akan menampilkan ini
+  else 
+    cout << "Unable to open file";
+  return 0;
 }
