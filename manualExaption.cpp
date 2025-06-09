ce9ecd99b6c1e9b4d8ff1fde9d438e6b10ab2329
zakibn18 <jekben815@gmail.com>
2025-06-09 18:51:04 +0700

    membuat manual exeption

--- manualExaption.cpp
+++ manualExaption.cpp
@@ -3,4 +3,22 @@ using namespace std;
 
 int main()
 {
+  try
+  {
+    cout << "Selamat Belajar di Prodi TI UMY" << endl;
+    throw("halo"); // melemparkan sebuah integer maka
+    cout << "Pernyataan tidak akan dieksekusi" << endl;
+  }
+  catch (int a)
+  {
+    // blok ini akan dieksekusi
+    cout << a << " : Pengecualian ini akan dieksekusi" << endl;
+  }
+  catch(...)
+  {
+    /*jika selain integer maka block ini akan dieksekusi*/
+    cout << "default Pengecualian dieksekusi" << endl;
+  }
+
+  return 0;
 }
