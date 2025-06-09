17ad658da668c9cc78fb1a98467ae7b9a735decc
zakibn18 <jekben815@gmail.com>
2025-06-09 17:29:45 +0700

    loop akan berhenti jika anda memasukkan karakter q

--- ioFile.cpp
+++ ioFile.cpp
@@ -20,6 +20,9 @@ int main()
     cout << "- ";
     // mendapatkan setiap karakter dalam satu baris
     getline(cin, baris);
+    // loop akan berhenti jika anda memasukkan karakter q
+    if (baris == "q")
+      break;
   }
   
 }
